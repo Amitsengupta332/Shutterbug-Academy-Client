09eaf02a315bc4d7bdb3a9b5a96e759a42cb41d3
Amitsen11 <89931804+Amitsen11@users.noreply.github.com>
2023-06-06 23:34:58 +0600

    first commit

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <button className="btn btn-primary">Default</button>
+        ,
+    },
+]);
