9c6349e8daa96d6c198641a5778def6f80c64251
Amitsen11 <89931804+Amitsen11@users.noreply.github.com>
2023-06-22 18:29:36 +0600

    done github public

--- src/hooks/useInstructor.jsx
+++ src/hooks/useInstructor.jsx
@@ -7,7 +7,7 @@ const useInstructor = () => {
     const [axiosSecure] = useAxiosSecure();
     const { data: isInstructor, isLoading: isInstructorLoading } = useQuery({
         queryKey: ['isInstructor', user?.email],
-        enabled: !loading,
+        enabled: !loading && !!user?.email && !!localStorage.getItem('access-token'),
         queryFn: async () => {
             const res = await axiosSecure.get(`/users/instructor/${user?.email}`);
             // console.log('is instructor response', res)
